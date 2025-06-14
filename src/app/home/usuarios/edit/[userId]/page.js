5e2f7e98107b9e1274e68e08b3a60ab56adcf03c
Bruno Paula Mendes <bruno.paulamendes@philips.com>
2025-06-14 16:52:48 -0300

    Update

--- src/app/home/usuarios/edit/[userId]/page.js
+++ src/app/home/usuarios/edit/[userId]/page.js
@@ -2,13 +2,13 @@
 
 import { useParams } from "next/navigation";
 import { useEdit } from "../hooks/useEdit";
-import { DatePicker, Form, Input, Radio, Space } from "antd";
+import { Button, DatePicker, Form, Input, Radio, Space } from "antd";
 import dayjs from "dayjs";
 
 export default function EditUser() {
     const params = useParams();
     const userId = params.userId;
-    const { user, formatDate } = useEdit({ userId });
+    const { user, formatDate, handleChange, handleSubmit } = useEdit({ userId });
     const birthDate = formatDate(user.birth_date || '');
 
     return (
@@ -20,25 +20,32 @@ export default function EditUser() {
                     labelCol={{ span: 4 }}
                     wrapperCol={{ span: 14 }}
                     layout="horizontal"
-                    style={{ maxWidth: 600 }}
+                    style={{ maxWidth: 600 }} 
+                    method="post"
+                    onSubmit={handleSubmit}
                 >
                     <Form.Item label="Name">
-                        <Input value={user.name} />
+                        <Input value={user.name} onChange={handleChange} />
                     </Form.Item>
                     <Form.Item label="E-mail">
-                        <Input value={user.email} />
+                        <Input value={user.email} onChange={handleChange} />
                     </Form.Item>
                     <Form.Item label="Birth date/Age">
                         <Space>
-                            <DatePicker value={dayjs(birthDate)} />
-                            <Input value={user.age} />
+                            <DatePicker value={dayjs(birthDate)} onChange={handleChange} />
+                            <Input value={user.age} onChange={handleChange} />
                         </Space>
                     </Form.Item>
                     <Form.Item label="Gender">
-                        <Radio.Group value={user.gender}>
+                        <Radio.Group value={user.gender} onChange={handleChange} > 
                             <Radio value="Masculino">Male</Radio>
                             <Radio value="Feminino">Female</Radio>
                         </Radio.Group>
+                    </Form.Item>
+                    <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+                        <Button type="primary" htmlType="submit" onClick={() => {handleSubmit}}>
+                            Submit
+                        </Button>
                     </Form.Item>
                 </Form>
             </div>
--- src/app/home/usuarios/edit/hooks/useEdit.js
+++ src/app/home/usuarios/edit/hooks/useEdit.js
@@ -41,8 +41,44 @@ export function useEdit({ userId } = {}) {
         return `${year}-${month}-${day}`;
     };
 
+    const handleChange = (e) => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        });
+
+        if (e.target.name === 'birth_date') {
+            const birth_date = new Date(e.target.value);
+            const today = new Date();
+            const age = today.getFullYear() - birth_date.getFullYear();
+            const monthDiff = today.getMonth() - birth_date.getMonth();
+            if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birth_date.getDate())) {
+                setUser({ ...user, birth_date: e.target.value, age: age - 1 });
+            } else {
+                setUser({ ...user, birth_date: e.target.value, age: age });
+            }
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        apiClient.put(`/usuarios/${userId}`, user, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('authToken')}`,
+            }
+        }).then((response) => {
+            console.log('User edited:', response.data);
+            router.push('/home/usuarios');
+        }).catch((error) => {
+            console.log('User edit error:', error.message);
+        });
+    };
+
     return {
         user,
-        formatDate
+        formatDate,
+        handleChange,
+        handleSubmit
     }
 }
